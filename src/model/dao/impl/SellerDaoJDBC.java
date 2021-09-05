0c3e39d4da424ca1be291a1b04bf993fba7e73cf
Pedro Gomes <you@example.com>
2021-09-05 10:46:18 -0300

    Saving seller

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -8,7 +8,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import com.mysql.jdbc.Statement;
+
 import db.DB;
 import db.DbException;
 import model.dao.SellerDao;
--- src/model/exception/ValidationException.java
+++ src/model/exception/ValidationException.java
@@ -3,6 +3,7 @@ package model.exception;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class ValidationException extends RuntimeException{
 
 	private static final long serialVersionUID = 1L;
